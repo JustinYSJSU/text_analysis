d88b17ec0f03bae6c1c790db7575ddab8850420e
JustinYSJSU <justin.yamamoto@sjsu.edu>
2023-08-09 14:38:57 -0700

    word freq for text

--- src/text_analysis.py
+++ src/text_analysis.py
@@ -1,13 +1,26 @@
 from analysis_tools import AnalysisTools
+from analyze_text import TextAnalysis
 
 analyzer = AnalysisTools()
+text_analyzer = TextAnalysis()
 
 class TextAnalysis():
   def __init__(self):
     pass
   
-  #Given user input from main.py, check if a path exists 
-  #If so, they have provided a file. If not, they have provided text
-  def analyze_text(user_input, self):
+  def analyze_text(self, user_input):
     print("Please pick an analysis option")
     analysis_choice = analyzer.analysis_options()
+
+    if analysis_choice == "1":
+      freq_dict = text_analyzer.word_freq_analysis_text(user_input)
+      print()
+      print("***Word Frequency Analysis Results***")
+      print()
+      
+      for word, freq in freq_dict.items():
+        print(f"{word}: {freq}")
+
+      print()
+      most_freq_word = list(freq_dict.keys())[0]
+      print(f"The most frequent word is: '{most_freq_word}' with a frequency of {freq_dict[most_freq_word]}")
